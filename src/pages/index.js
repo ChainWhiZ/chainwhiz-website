09f7b19003182a3fd7506d0791046d36be1c3c11
akp111 <pradhanashish95@gmail.com>
2022-07-03 18:21:18 +0530

    fix:small animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <Landing />
     </div>
